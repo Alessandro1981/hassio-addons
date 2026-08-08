ddc2cbecdd57fdc4e49dc99667604eabb62be6ad
Alessandro Bacioccola <36128184+Alessandro1981@users.noreply.github.com>
2026-08-08 23:35:07 +0200

    Export normalized provider payloads

--- fitness_data_hub/src/providers/__init__.py
+++ fitness_data_hub/src/providers/__init__.py
@@ -1,4 +1,5 @@
 from .base import FitnessProvider
 from .factory import get_provider
+from .models import ProviderActivity, ProviderAthlete
 
-__all__ = ["FitnessProvider", "get_provider"]
+__all__ = ["FitnessProvider", "ProviderActivity", "ProviderAthlete", "get_provider"]
